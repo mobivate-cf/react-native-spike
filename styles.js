dfbd35cf698fe1daa7528b76e913526f809ab4fb
Chris Kozlowski <kozlowskicd@gmail.com>
2019-07-10 17:58:03 -0700

    Added state, buttons, if keyboardView, and header and if components

---
+++ styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    top: 1,
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerText: {
+    fontSize: 80,
+  }
+});
+
+export default styles;
