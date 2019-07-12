337ac267b5c7e4173a6c083ad1c86482b3a76d8b
Chris Kozlowski <kozlowskicd@gmail.com>
2019-07-11 17:49:39 -0700

    finish oauth implementation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, KeyboardAvoidingView, Button, TextInput } from 'react-native';
+import { AuthSession, WebBrowser, Linking } from 'expo';
 
 import styles from './styles';
 import If from './components/if/if';
@@ -21,12 +22,35 @@ export default class App extends React.Component {
     });
   }
 
-  login = () => {
-    if(!this.state.isLoggedIn) {
-      this.setState({
-        isLoggedIn: true
-      });
+  handleRedirect = async event => {
+    WebBrowser.dismissBrowser()
+  }
+
+  handleOAuthLogin = async () => {
+    // gets the app's deep link
+    let redirectUrl = await Linking.getInitialURL()
+    // this should change depending on where the server is running
+    let authUrl = 'https://mobivate-server.herokuapp.com/login/twitter';
+    this.addLinkingListener();
+    try {
+      let authResult = await WebBrowser.openAuthSessionAsync('https://mobivate-server.herokuapp.com/login/twitter', redirectUrl)
+      await this.setState({ authResult });
+      console.log({authResult})
+      if(authResult.type !== 'cancel') {
+        this.setState({isLoggedIn: true})
+      }
+    } catch (err) {
+      console.log('ERROR:', err)
     }
+  this.removeLinkingListener()
+  }
+
+  addLinkingListener = () => {
+    Linking.addEventListener('url', this.handleRedirect)
+  }
+
+  removeLinkingListener = () => {
+    Linking.removeEventListener('url', this.handleRedirect)
   }
 
   logout = () => {
@@ -44,10 +68,11 @@ export default class App extends React.Component {
       <AppHeader />
       <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
         <If condition={!this.state.isLoggedIn}>
-          <Button onPress={this.login} title='Login with Twitter' />
+          <Button onPress={this.handleOAuthLogin} title='Login with Twitter' />
         </If>
         <If condition={this.state.isLoggedIn}>
           <Text style={{ fontFamily: 'Montserrat' }}>Logged In!</Text>
+          {/* <Text>{}</Text> */}
           <TextInput
             style={{height: 40, width: 90, borderColor: 'gray', borderWidth: 1}}
             onChangeText={(text) => this.setState({text})}
